f093816f8eb1e6bc2833af5abf7cb7ab6bf53d9a
Silvia <silvercam15>
2019-03-01 14:57:06 -0600

    more stuff!

--- Golang/server.go
+++ Golang/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"flag"
 	"log"
 	"net/http"
@@ -11,13 +13,34 @@ var (
 	dir    = flag.String("dir", ".", "directory to serve")
 )
 
+func render(w http.ResponseWriter, tmpl string) {
+    tmpl = fmt.Sprintf("templates/%s", tmpl)
+    t, err := template.ParseFiles(tmpl)
+    if err != nil {
+        log.Print("template parsing error: ", err)
+    }
+    err = t.Execute(w, "")
+    if err != nil {
+        log.Print("template executing error: ", err)
+    }
+}
+
+func Home(w http.ResponseWriter, req *http.Request) {
+    render(w, "index.html")
+}
+
 func Game(w http.ResponseWriter, req *http.Request) {
     render(w, "game.html")
 }
 
 func main() {
-	flag.Parse()
-	log.Printf("listening on %q...", *listen)
+//	flag.Parse()
+//	log.Printf("listening on %q...", *listen)
+	http.HandleFunc("/", Home)
   http.HandleFunc("/game/", Game)
-	log.Fatal(http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir))))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+	}
+//	log.Fatal(http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir))))
 }
