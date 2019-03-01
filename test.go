3ba9a2fbe02faf7a53db58b0cc59b7f048dda025
Joey DiMaria <jdimaria@nd.edu>
2019-03-01 18:28:11 -0500

    post question

--- test.go
+++ test.go
@@ -15,11 +15,11 @@ func main() {
 }
 
 func MakeRequest() {
+/*
 	name := "manuel"
 	formData := url.Values{
 		"name": {name},
 	}
-
 
 	resp, err := http.PostForm("http://localhost:8000/start", formData)
 	if err != nil {
@@ -31,37 +31,21 @@ func MakeRequest() {
 	json.NewDecoder(resp.Body).Decode(&result)
 
 	log.Println(result["form"])
-/*
-	message := map[string]interface{}{
-		"name": "poopyhead",
-		"hello": "world",
-		"life":  42,
-		"embedded": map[string]string{
-			"yes": "of course!",
-		},
-	}
-	fmt.Println(message)
-	fmt.Println("")
+*/
 
-	bytesRepresentation, err := json.Marshal(message)
+	question := "what is your iq?"
+	formData := url.Values{
+		"question": {question},
+	}
+
+	resp, err := http.PostForm("http://localhost:8000/game", formData)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(bytesRepresentation)
-	fmt.Println("")
-
-	resp, err := http.Post("http://localhost:8000/start", "application/json", bytes.NewBuffer(bytesRepresentation))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(resp)
-	fmt.Println("")
 
 	var result map[string]interface{}
 
 	json.NewDecoder(resp.Body).Decode(&result)
 
-	log.Println(result)
-	log.Println(result["data"])
-*/
+	log.Println(result["form"])
 }
