af009bb3c841673802d87e5edba15057994c3a0f
Manuel Marroquin <mmarroqu@nd.edu>
2019-03-01 16:56:52 -0500

    go get github.com/gorilla/mux to run

--- server.go
+++ server.go
@@ -1,15 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "encoding/json"
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+    "fmt"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello World")
-	})
+var players [] string
+var questions [] string
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+// our main function
+func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/start", PostPlayer).Methods("POST")
+    router.HandleFunc("/game/", GetQuestion).Methods("GET")
+    router.HandleFunc("/game/", SendQuestion).Methods("POST")
+
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+
+func PostPlayer(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    var person string
+    _ = json.NewDecoder(r.Body).Decode(&person)
+    fmt.Println(params["name"])
+
+  
+}
+
+
+func GetQuestion(w http.ResponseWriter, r *http.Request) {
+   
+}
+
+func SendQuestion(w http.ResponseWriter, r *http.Request) {
+
 }
