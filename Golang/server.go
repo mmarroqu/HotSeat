cfa99c480992aff99c49cf4051ce4efc1fc06cea
Silvia <silvercam15>
2019-03-01 12:15:04 -0600

    Silvia's Golang server

---
+++ Golang/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	listen = flag.String("listen", ":8080", "listen address")
+	dir    = flag.String("dir", ".", "directory to serve")
+)
+
+func Game(w http.ResponseWriter, req *http.Request) {
+    render(w, "game.html")
+}
+
+func main() {
+	flag.Parse()
+	log.Printf("listening on %q...", *listen)
+  http.HandleFunc("/game/", Game)
+	log.Fatal(http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir))))
+}
