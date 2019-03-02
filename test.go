f6542adedda7c6541e22cea6fc31407084c2bb7c
Joey DiMaria <jdimaria@nd.edu>
2019-03-01 19:04:19 -0500

    added test for get request, reformatted into functions, added comments

--- test.go
+++ test.go
@@ -4,18 +4,25 @@ import (
 	"net/http"
 	"net/url"
 	"log"
-	//"io/ioutil"
+	"io/ioutil"
 	"encoding/json"
-	//"bytes"
-	//"fmt"
+	"fmt"
 )
 
 func main() {
-	MakeRequest()
+	//adds user name
+	TestPostName()
+	//adds two questions
+	TestPostQuestion()
+	TestPostQuestion()
+	//gets the two questions and tries one extra time
+	TestGetQuestion()
+	TestGetQuestion()
+	TestGetQuestion()
 }
 
-func MakeRequest() {
-/*
+//tests the post request for sending a user's name
+func TestPostName() {
 	name := "manuel"
 	formData := url.Values{
 		"name": {name},
@@ -31,8 +38,10 @@ func MakeRequest() {
 	json.NewDecoder(resp.Body).Decode(&result)
 
 	log.Println(result["form"])
-*/
+}
 
+//tests the post request for submitting a question
+func TestPostQuestion() {
 	question := "what is your iq?"
 	formData := url.Values{
 		"question": {question},
@@ -48,4 +57,19 @@ func MakeRequest() {
 	json.NewDecoder(resp.Body).Decode(&result)
 
 	log.Println(result["form"])
+}
+
+//tests the get request for getting a question
+func TestGetQuestion() {
+	response, err := http.Get("http://localhost:8000/game")
+    if err != nil {
+		log.Fatalln(err) 
+	} else {
+   		defer response.Body.Close()
+        contents, err := ioutil.ReadAll(response.Body)
+        if err != nil {
+			log.Fatalln(err)
+		}
+        fmt.Printf("%s", string(contents))
+    }
 }
